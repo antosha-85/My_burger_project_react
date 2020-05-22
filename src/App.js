b331b8d8773934bc8bf339783c0feb3794ad5bc5
Anton Smirnov <antosha-85@yandex.ru>
2020-05-21 22:21:31 -0600

    Created a new layout component and wrapped it inside Aux component, which is a higher order function

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React from 'react';
-
-function App() {
-  return (
-    <div>
-      
-    </div>
-  );
+import React, { Component } from 'react'
+import Layout from './components/Layout/Layout'
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Layout>
+          <p>Test</p>
+        </Layout>
+      </div>
+    )
+  }
 }
-
-export default App;
