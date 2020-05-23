e8b7cd7b48e71fb1ad1c9b6e5c32b34867431d0a
Anton Smirnov <antosha-85@yandex.ru>
2020-05-22 23:44:42 -0600

    Added state to BurgerBuilder and then inside Burger.js I transformed an object into an array and then I rendered those ingredients number of times equal to those ingredient

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,11 +3,25 @@ import classes from "./Burger.module.css";
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 const Burger = (props) => {
+  //creates an array of keys and then we're mapping it out
+  const transformedIngredients = Object.keys(props.ingredients).map(
+    ingredientKey => {
+      //first return will bring us an array of arrays with elements number equal to ingredients number
+      return [...Array(props.ingredients[ingredientKey])].map((_, i) => {
+        return (
+          <BurgerIngredient key={ingredientKey + i} type={ingredientKey} />
+        );
+      });
+    }
+  );
+  console.log((transformedIngredients).map(
+    ingredientKey => {
+      //first return will bring us an array of elements equal to 
+      return [...Array(props.ingredients[ingredientKey])]}));
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
-      <BurgerIngredient type="cheese" />
-      <BurgerIngredient type="meat" />
+      {transformedIngredients}
       <BurgerIngredient type="bread-bottom" />
     </div>
   );
