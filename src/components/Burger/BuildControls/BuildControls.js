3892d4791ed43fbbe058331fec3790372c77f7d3
Anton Smirnov <antosha-85@yandex.ru>
2020-05-23 22:12:32 -0600

    Created addIngredient handler method, added totalPrice to our state, updated state and passed that inside buildControls and BuildContol without useContext so far

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,17 +1,22 @@
 import React from "react";
 import classes from "./BuildControls.module.css";
-import BuildControl from './BuildControl/BuildControl'
+import BuildControl from "./BuildControl/BuildControl";
 const controls = [
-    {label: 'Salad', type: 'salad'},
-    {label: 'Bacon', type: 'bacon'},
-    {label: 'Cheese', type: 'cheese'},
-    {label: 'Meat', type: 'meat'}
-]
+  { label: "Salad", type: "salad" },
+  { label: "Bacon", type: "bacon" },
+  { label: "Cheese", type: "cheese" },
+  { label: "Meat", type: "meat" },
+];
 const BuildControls = (props) => (
-<div className={classes.BuildControls}>
-{controls.map(control => (
-    <BuildControl key={control.label} label={control.label}/>
-))}
-</div>);
+  <div className={classes.BuildControls}>
+    {controls.map((control) => (
+      <BuildControl 
+      key={control.label} 
+      label={control.label} 
+    //   type={control.type}
+      added={()=> props.ingredientAdded(control.type)}/>
+    ))}
+  </div>
+);
 
 export default BuildControls;
