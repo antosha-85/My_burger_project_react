b3dd2030c0a622865becb5fe412a248eb5e924fb
Anton Smirnov <antosha-85@yandex.ru>
2020-05-26 22:47:43 -0600

    Made logo be responsive using 11 and 80% height

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -6,7 +6,11 @@ import classes from "./Toolbar.module.css";
 const Toolbar = (props) => (
   <header className={classes.Toolbar}>
     <div>MENU</div>
+    <div className={classes.Logo}>
+
+    {/* <Logo height='80%' /> */}
     <Logo />
+    </div>
     <nav>
       <NavigationItems />
     </nav>
