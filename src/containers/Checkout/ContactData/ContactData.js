16d02511d272118cb01e4c2b193ff910ec3df777
Anton Smirnov <antosha-85@yandex.ru>
2020-06-12 23:30:17 -0600

    Created a form validity for required, min and max length by creating a checkValidity method that either returns true or false and then using it inside inputChangedHandler, we still have a bug o f that we need to combine checks and not use them one after another

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -15,6 +15,10 @@ class ContactData extends Component {
           placeholder: "Your name",
         },
         value: "",
+        validation: {
+          required: true,
+        },
+        valid: false,
       },
       street: {
         elementType: "input",
@@ -23,6 +27,10 @@ class ContactData extends Component {
           placeholder: "Your street",
         },
         value: "",
+        validation: {
+          required: true,
+        },
+        valid: false,
       },
       zipCode: {
         elementType: "input",
@@ -31,6 +39,12 @@ class ContactData extends Component {
           placeholder: "Zip Code",
         },
         value: "",
+        validation: {
+          required: true,
+          minLength: 5,
+          maxLength: 5
+        },
+        valid: false,
       },
       country: {
         elementType: "input",
@@ -39,6 +53,10 @@ class ContactData extends Component {
           placeholder: "Your Country",
         },
         value: "",
+        validation: {
+          required: true,
+        },
+        valid: false,
       },
       email: {
         elementType: "input",
@@ -47,6 +65,10 @@ class ContactData extends Component {
           placeholder: "Your email",
         },
         value: "",
+        validation: {
+          required: true,
+        },
+        valid: false,
       },
       deliveryMethod: {
         elementType: "select",
@@ -62,22 +84,38 @@ class ContactData extends Component {
     loading: false,
   };
 
+  checkValidity = (value, rules) => {
+    let isValid = false;
+
+    if (rules.required) {
+      isValid = value.trim() !== '';
+    }
+    if(rules.minLength) {
+      isValid = value.length >= rules.minLength
+    }
+    if(rules.maxLength) {
+      isValid = value.length <= rules.maxLength
+    }
+    return isValid
+  };
   orderHandler = (e) => {
     e.preventDefault();
     this.setState({ loading: true });
-    const formData = {}
+    const formData = {};
     for (let formElementIdentifier in this.state.orderForm) {
-      formData[formElementIdentifier] = this.state.orderForm[formElementIdentifier].value
+      formData[formElementIdentifier] = this.state.orderForm[
+        formElementIdentifier
+      ].value;
     }
     const order = {
       ingredients: this.props.ingredients,
       price: this.props.price,
-      orderData: formData
+      orderData: formData,
     };
     axios
       .post("/orders.json", order)
       .then((response) => {
-        console.log('response', response)
+        console.log("response", response);
         this.setState({ loading: false });
         this.props.history.push("/");
       })
@@ -87,12 +125,14 @@ class ContactData extends Component {
   };
 
   inputChangedHadler = (e, inputIdentifier) => {
-    const updatedOrderForm = {...this.state.orderForm}
-    const updatedFormElement = {...updatedOrderForm[inputIdentifier]}
-    updatedFormElement.value = e.target.value
-    updatedOrderForm[inputIdentifier] = updatedFormElement
-    this.setState({orderForm: updatedOrderForm})
-  }
+    const updatedOrderForm = { ...this.state.orderForm };
+    const updatedFormElement = { ...updatedOrderForm[inputIdentifier] };
+    updatedFormElement.value = e.target.value;
+    updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation)
+    updatedOrderForm[inputIdentifier] = updatedFormElement;
+    console.log('updatedFormElement', updatedFormElement)
+    this.setState({ orderForm: updatedOrderForm });
+  };
   render() {
     const formElementsArray = [];
     for (let key in this.state.orderForm) {
@@ -101,21 +141,19 @@ class ContactData extends Component {
         config: this.state.orderForm[key],
       });
     }
-    
+
     let form = (
       <form onSubmit={this.orderHandler}>
         {formElementsArray.map((formElement) => (
-          <Input 
-          key={formElement.id}
-          elementType={formElement.config.elementType} 
-          elementConfig={formElement.config.elementConfig}
-          value={formElement.config.value}
-          changed={(event)=>this.inputChangedHadler(event, formElement.id)}
+          <Input
+            key={formElement.id}
+            elementType={formElement.config.elementType}
+            elementConfig={formElement.config.elementConfig}
+            value={formElement.config.value}
+            changed={(event) => this.inputChangedHadler(event, formElement.id)}
           />
         ))}
-        <Button btnType="Success">
-          ORDER
-        </Button>
+        <Button btnType="Success">ORDER</Button>
       </form>
     );
     if (this.state.loading) {
@@ -125,7 +163,6 @@ class ContactData extends Component {
       <div className={classes.ContactData}>
         <h4>Enter your Contact Data</h4>
         {form}
-        
       </div>
     );
   }
