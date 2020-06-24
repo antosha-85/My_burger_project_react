6ca72e5ba9c33b42c3093c634083e71e2cabfa82
Anton Smirnov <antosha-85@yandex.ru>
2020-06-23 22:30:14 -0600

    Added extension for redux

--- src/index.js
+++ src/index.js
@@ -5,10 +5,13 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { Provider } from "react-redux";
-import { createStore } from 'redux'
-import reducer from './store/reducer'
+import { createStore } from "redux";
+import reducer from "./store/reducer";
 
-const store = createStore(reducer)
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 const app = (
   <Provider store={store}>
     <BrowserRouter>
