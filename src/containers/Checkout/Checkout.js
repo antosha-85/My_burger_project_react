7c5b592867750fd995c32fdbbe4e5a77d74dc9a7
Anton Smirnov <antosha-85@yandex.ru>
2020-06-30 23:48:33 -0600

    Added redirect to a checkout.js file and added if statement to render summary if there are ingredients

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Route } from "react-router-dom";
+import { Route, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 import CheckoutSummary from "../../components/Order/CheckoutSummary/CheckoutSummary";
 import ContactData from "./ContactData/ContactData";
@@ -31,26 +31,35 @@ class Checkout extends Component {
     this.props.history.replace("checkout/contact-data");
   };
   render() {
-    return (
-      <div>
-        <CheckoutSummary
-          ingredients={this.props.ings}
-          checkoutCancelled={this.checkoutCancelledHandler}
-          checkoutContinued={this.checkoutContinuedHandler}
-        />
-        <Route
-          path={`${this.props.match.url}/contact-data`}
-          // render={(props) => (
-          //   <ContactData
-          //     ingredients={this.props.ings}
-          //     price={this.props.price}
-          //     {...props}
-          //   />
-          // )}
-          component={ContactData}
-        />
-      </div>
-    );
+    let summary = <Redirect to="/" />;
+    if (this.props.ings) {
+      summary = (
+        <div>
+          <CheckoutSummary
+            ingredients={this.props.ings}
+            checkoutCancelled={this.checkoutCancelledHandler}
+            checkoutContinued={this.checkoutContinuedHandler}
+          />
+          <Route
+            path={`${this.props.match.url}/contact-data`}
+            // render={(props) => (
+            //   <ContactData
+            //     ingredients={this.props.ings}
+            //     price={this.props.price}
+            //     {...props}
+            //   />
+            // )}
+            component={ContactData}
+          />
+        </div>
+      );
+    }
+    return summary;
+    // {/* <CheckoutSummary
+    //   ingredients={this.props.ings}
+    //   checkoutCancelled={this.checkoutCancelledHandler}
+    //   checkoutContinued={this.checkoutContinuedHandler}
+    // /> */}
   }
 }
 
