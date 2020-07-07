382a08b5efe5893f6975fd6fa76b52fe1aa95ed1
Anton Smirnov <antosha-85@yandex.ru>
2020-07-06 23:42:17 -0600

    Added an email check using regExpressions

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -53,6 +53,10 @@ class Auth extends Component {
     if (rules.maxLength) {
       isValid = value.length <= rules.maxLength && isValid;
     }
+    if (rules.isEmail) {
+      const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+      isValid = pattern.test(value) && isValid
+  }
     return isValid;
   };
 
