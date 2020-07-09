541fbb3b9970ed1d162cfc9fa3bb4eb737ce0012
Anton Smirnov <antosha-85@yandex.ru>
2020-07-08 23:36:43 -0600

    Added spinner for authenticaiton

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
 import Input from "../../components/UI/Input/Input";
 import Button from "../../components/UI/Button/Button";
+import Spinner from "../../components/UI/Spinner/Spinner";
 import classes from "./Auth.module.css";
-import * as actions from '../../store/actions/index'
-import {connect} from 'react-redux'
+import * as actions from "../../store/actions/index";
+import { connect } from "react-redux";
 
 class Auth extends Component {
   state = {
@@ -42,8 +43,8 @@ class Auth extends Component {
 
   checkValidity = (value, rules) => {
     let isValid = true;
-    if(!rules) {
-      return true
+    if (!rules) {
+      return true;
     }
 
     if (rules.required) {
@@ -57,8 +58,8 @@ class Auth extends Component {
     }
     if (rules.isEmail) {
       const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
-      isValid = pattern.test(value) && isValid
-  }
+      isValid = pattern.test(value) && isValid;
+    }
     return isValid;
   };
 
@@ -78,18 +79,22 @@ class Auth extends Component {
     this.setState({ controls: upadatedControls });
   };
 
-submitHandler = (event) => {
-  event.preventDefault();
-  this.props.onAuth(this.state.controls.email.value, this.state.controls.password.value, this.state.isSignup)
-}
+  submitHandler = (event) => {
+    event.preventDefault();
+    this.props.onAuth(
+      this.state.controls.email.value,
+      this.state.controls.password.value,
+      this.state.isSignup
+    );
+  };
 
-swithAuthModeHandler = () => {
-  this.setState(prevState => {
-    return {
-      isSignup: !prevState.isSignup
-    }
-  })
-}
+  swithAuthModeHandler = () => {
+    this.setState((prevState) => {
+      return {
+        isSignup: !prevState.isSignup,
+      };
+    });
+  };
   render() {
     const formElementsArray = [];
     for (let key in this.state.controls) {
@@ -98,25 +103,30 @@ swithAuthModeHandler = () => {
         config: this.state.controls[key],
       });
     }
-    const form = formElementsArray.map((formElement) => (
-        <Input
-          key={formElement.id}
-          element={formElement.id}
-          elementType={formElement.config.elementType}
-          elementConfig={formElement.config.elementConfig}
-          value={formElement.config.value}
-          invalid={!formElement.config.valid}
-          shouldValidate={formElement.config.validation}
-          touched={formElement.config.touched}
-          changed={(event) => this.inputChangedHandler(event, formElement.id)}
-        />
+    let form = formElementsArray.map((formElement) => (
+      <Input
+        key={formElement.id}
+        element={formElement.id}
+        elementType={formElement.config.elementType}
+        elementConfig={formElement.config.elementConfig}
+        value={formElement.config.value}
+        invalid={!formElement.config.valid}
+        shouldValidate={formElement.config.validation}
+        touched={formElement.config.touched}
+        changed={(event) => this.inputChangedHandler(event, formElement.id)}
+      />
     ));
+
+    if (this.props.loading) {
+      form = <Spinner />;
+    }
     return (
       <div className={classes.Auth}>
         <form onSubmit={this.submitHandler}>
           {form}
           <Button btnType="Success">Submit</Button>
-          <Button btnType="Danger" clicked={this.swithAuthModeHandler}>SWITCH TO {this.state.isSignup ? 'SIGNIN' : 'SIGNUP'}
+          <Button btnType="Danger" clicked={this.swithAuthModeHandler}>
+            SWITCH TO {this.state.isSignup ? "SIGNIN" : "SIGNUP"}
           </Button>
         </form>
       </div>
@@ -124,9 +134,15 @@ swithAuthModeHandler = () => {
   }
 }
 
-const mapDispatchToProps = dispatch => {
+const mapStateToProps = (state) => {
   return {
-    onAuth: (email, password, isSignup) => dispatch(actions.auth(email, password, isSignup))
-  }
-}
-export default connect(null, mapDispatchToProps) (Auth);
+    loading: state.auth.loading,
+  };
+};
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onAuth: (email, password, isSignup) =>
+      dispatch(actions.auth(email, password, isSignup)),
+  };
+};
+export default connect(mapStateToProps, mapDispatchToProps)(Auth);
