202afb517a0f6ba36f016d9d32d47a38be787c07
Anton Smirnov <antosha-85@yandex.ru>
2020-07-11 00:27:42 -0600

    Added conditional statement on Orders nav link, if authenticated then we can see the Orders tabs, otherwise we do not

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -8,7 +8,7 @@ const NavigationItems = props => (
     <NavigationItem link='/' exact
     // active don't need this class because of NavLink
     >Burger Builder</NavigationItem>
-    <NavigationItem link='/orders' >Orders</NavigationItem>
+    {props.isAuthenticated ? <NavigationItem link='/orders' >Orders</NavigationItem> : null}
    {!props.isAuthenticated ? 
    <NavigationItem link='/auth' >Authenticate</NavigationItem> : 
    <NavigationItem link='/logout' >Logout</NavigationItem> }
