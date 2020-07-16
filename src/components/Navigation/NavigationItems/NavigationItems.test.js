8c2efa3dc0f40b524cc2d64e48848510e0f0c99b
Anton Smirnov <antosha-85@yandex.ru>
2020-07-15 21:15:18 -0600

    Added another test for 3 elements rendered if authenticated and implemented beforeEach method plus setProps

--- src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -1,16 +1,25 @@
 import React from "react";
 import NavigationItems from "./NavigationItems";
-import NavigationItem from './NavigationItem/NavigationItem'
+import NavigationItem from "./NavigationItem/NavigationItem";
 import { configure, shallow } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 configure({
   adapter: new Adapter(),
 });
 
-
 describe("<NavigationItems/>", () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(<NavigationItems />);
+  });
   it("should render two <NavigationItem/> elements if not authnticated", () => {
-    const wrapper = shallow(<NavigationItems />);
-    expect(wrapper.find(NavigationItem)).toHaveLength(2)
+    // const wrapper = shallow(<NavigationItems />);
+    expect(wrapper.find(NavigationItem)).toHaveLength(2);
+  });
+  it("should render three <NavigationItem/> elements if authnticated", () => {
+    // const wrapper = shallow(<NavigationItems isAuthenticated />);
+    // wrapper = shallow(<NavigationItems isAuthenticated/>) alternative of wrapper.setProps({object with key-value pairs for props})
+    wrapper.setProps({isAuthenticated: true})
+    expect(wrapper.find(NavigationItem)).toHaveLength(3);
   });
 });
