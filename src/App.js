141f4c0020dcf372ba84e83b3626852584fe3299
Anton Smirnov <antosha-85@yandex.ru>
2020-08-04 23:13:01 -0600

    Changed async components with React.lazy and changed components with render methods

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component, useEffect } from "react";
+import React, { Component, useEffect, Suspense } from "react";
 import Layout from "./hoc/Layout/Layout";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 // import Checkout from "./containers/Checkout/Checkout";
@@ -8,15 +8,24 @@ import { Route, Switch, withRouter, Redirect } from "react-router-dom";
 import Logout from "./containers/Auth/Logout/Logout";
 import { connect } from "react-redux";
 import * as actions from "./store/actions/index";
-import asyncComponent from "./hoc/asyncComponent/asyncComponent";
+// import asyncComponent from "./hoc/asyncComponent/asyncComponent";
 
-const asyncCheckout = asyncComponent(() => {
+// const asyncCheckout = asyncComponent(() => {
+//   return import("./containers/Checkout/Checkout");
+// });
+// const asyncOrders = asyncComponent(() => {
+//   return import("./containers/Orders/Orders");
+// });
+// const asyncAuth = asyncComponent(() => {
+//   return import("./containers/Auth/Auth");
+// });
+const Checkout = React.lazy(() => {
   return import("./containers/Checkout/Checkout");
 });
-const asyncOrders = asyncComponent(() => {
+const Orders = React.lazy(() => {
   return import("./containers/Orders/Orders");
 });
-const asyncAuth = asyncComponent(() => {
+const Auth = React.lazy(() => {
   return import("./containers/Auth/Auth");
 });
 const App = (props) => {
@@ -28,7 +37,10 @@ const App = (props) => {
   }, []);
   let routes = (
     <Switch>
-      <Route path="/auth" component={asyncAuth} />
+      <Route path="/auth" 
+      // component={asyncAuth} 
+      render={(()=> <Auth/>)}
+      />
       <Route path="/" exact component={BurgerBuilder} />
       <Redirect to="/" />
     </Switch>
@@ -37,10 +49,10 @@ const App = (props) => {
   if (props.isAuthenticated) {
     routes = (
       <Switch>
-        <Route path="/checkout" component={asyncCheckout} />
-        <Route path="/orders" component={asyncOrders} />
+        <Route path="/checkout" render={(()=> <Checkout/>)} />
+        <Route path="/orders" render={(()=> <Orders/>)} />
         <Route path="/logout" component={Logout} />
-        <Route path="/auth" component={asyncAuth} />
+        <Route path="/auth" render={(()=> <Auth/>)} />
         <Route path="/" exact component={BurgerBuilder} />
         <Redirect to="/" />
       </Switch>
@@ -48,7 +60,7 @@ const App = (props) => {
   }
   return (
     <div>
-      <Layout>{routes}</Layout>
+      <Layout><Suspense fallback={<p>Loading...</p>}>{routes}</Suspense></Layout>
     </div>
   );
 };
