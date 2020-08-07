690af27a0dccf85afba43f46c0f0e16f905df928
Anton Smirnov <antosha-85@yandex.ru>
2020-08-06 21:53:15 -0600

    Updated Logout.js with hooks

--- src/containers/Auth/Logout/Logout.js
+++ src/containers/Auth/Logout/Logout.js
@@ -1,14 +1,13 @@
-import React, { Component } from 'react'
+import React, { useEffect } from 'react'
 import * as actions from '../../../store/actions/index'
 import { Redirect } from 'react-router-dom'
 import {connect} from 'react-redux'
-class Logout extends Component {
-    componentDidMount() {
-        this.props.onLogout()
-    }
-    render() {
+const Logout = props => {
+    useEffect(()=> {
+        props.onLogout()
+
+    }, [])
         return <Redirect to='/'/>
-    }
 }
 
 const mapDispatchToProps = dispatch => {
